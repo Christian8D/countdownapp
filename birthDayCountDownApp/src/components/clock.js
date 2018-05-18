9a7843d5b863a1170b518371490df043979bfa53
Christian Cosio <christiancosio@Christians-MBP.attlocal.net>
2018-05-18 08:29:46 -0700

    implemented age feature

--- birthDayCountDownApp/src/components/clock.js
+++ birthDayCountDownApp/src/components/clock.js
@@ -1,24 +1,77 @@
 import React, { Component } from 'react';
 
- class clock extends Component{
+class Clock extends Component {
 
-    constructor (props){
+
+    constructor(props) {
         super(props)
 
-        this.state ={
-            timeReminding:{}
+        this.timer = 0
+        this.birthday = props.birthdayFormState.startDate.toString();
+        this.getTimeRemaining = this.getTimeRemaining.bind(this);
+
+        this.state = {
+            timeRemaining: this.getTimeRemaining(props.birthdayFormState.startDate.toString())
+        }
+
+    }
+
+    getTimeRemaining(birthday) {
+        var bday = new Date(birthday);
+        let today = new Date();
+
+        var distance = bday.getTime() - today.getTime();
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        return {
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
         }
     }
-    render() {
- const data = this.state.timeReminding
-       return(
-           <div>
-               <div>DAYS {data.days}</div>
-               <div>HRS {data.hours}</div>
-               <div>MINS {data.minutes}</div>
-               <div>SECS {data.seconds}</div>
-           </div>
-       )
+
+    getAge = function() {
+        var bday = new Date(this.birthday);
+        let today = new Date();
+
+        var distance = today.getTime() - bday.getTime();
+        var daysOld = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var yearsOld = Number((daysOld/365).toFixed(0)); 
+
+        return yearsOld
+    }.bind(this)
+
+    componentDidMount() {
+     
+            this.timer = setInterval(() => {
+                const timeRemaining = this.getTimeRemaining(this.birthday)
+                this.setState({ timeRemaining: timeRemaining })
+            }, 1000);
+ 
     }
+
+    render() {  
+        const data = this.state.timeRemaining
+        return (
+            <div>
+                <div>
+                    <div>DAYS {data.days}</div>
+                    <div>HRS {data.hours}</div>
+                    <div>MINS {data.minutes}</div>
+                    <div>SECS {data.seconds}</div>
+                </div>
+                <div>
+                    {<h4>remaining until you are {this.getAge()}</h4>}
+                </div>
+            </div>
+        )
+    }
+
 }
-export default clock;
+
+export default Clock;
